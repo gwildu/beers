b0783fcf205b994f6ebcbc525acd2690222d7e3e
gwildu <gwildu@gwildu.ch>
2016-12-19 06:26:00 +0100

    adjusted loading of assetMap

--- server/utils/view-helpers.js
+++ server/utils/view-helpers.js
@@ -3,15 +3,16 @@ var exists = require('fs').accessSync
 var path = require('path')
 var manifest = path.join(path.resolve('.'), 'dist/asset-manifest.json')
 var isProd = process.env.NODE_ENV === 'production'
+var assetMap = isProd ? JSON.parse(readFile(manifest, 'utf-8')) : ''
 
 // preload asset-manifest.json if exists
-if (isProd) {
-  try {
-    exists(manifest)
-    var assetMap = JSON.parse(readFile(manifest, 'utf-8'))
-  } catch (e) {
-  }
-}
+// if (isProd) {
+//   try {
+//     exists(manifest)
+//     var assetMap = JSON.parse(readFile(manifest, 'utf-8'))
+//   } catch (e) {
+//   }
+// }
 
 module.exports = {
   asset: function (path) {
